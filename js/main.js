3d609780944816478c5f301beb6b960f34dbcf93
Arda Acar <arda@namecial.com>
2022-11-26 17:59:36 +0300

    Menu color issue fixed

--- js/main.js
+++ js/main.js
@@ -6,8 +6,10 @@ targets.forEach(element => {
     var target = document.querySelector(element.dataset.target)
     targets.forEach(element2 => {
       var target2 = document.querySelector(element2.dataset.target)
+      element2.style.color = 'var(--menu_text_color)'
       target2.style.display = 'none'
     });
-    target.style.display= 'flex';
+    element.style.color = 'var(--menu_active_text_color)'
+    target.style.display= 'flex'
   })
-});
+})
